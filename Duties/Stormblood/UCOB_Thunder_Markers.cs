be2f6debc46bc54c0166ce1f972d4905119f085c
ssalazar217 <ssalazar217@users.noreply.github.com>
2026-04-12 22:33:55 -0500

    Ultra-compatible syntax fix for compilation errors

--- Duties/Stormblood/UCOB_Thunder_Markers.cs
+++ Duties/Stormblood/UCOB_Thunder_Markers.cs
@@ -9,106 +9,105 @@ using ECommons;
 using Splatoon.SplatoonScripting;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
-namespace SplatoonScriptsOfficial.Duties.Stormblood;
-
-public class UCOB_Thunder_Markers : SplatoonScript
+namespace SplatoonScriptsOfficial.Duties.Stormblood
 {
-    public override HashSet<uint> ValidTerritories => null; 
-    public override Metadata? Metadata => new(1, "Cant'Load");
-
-    private Config Conf => Controller.GetConfig<Config>();
-    private const uint THUNDER_ID = 466;
-    private const uint TEST_ID = 50; 
-    private HashSet<uint> _markedPlayers = new();
-    private bool _active = false;
-
-    public override void OnSettingsDraw()
+    public class UCOB_Thunder_Markers : SplatoonScript
     {
-        ImGui.Checkbox("Habilitar marcado automatico de Thunder", ref Conf.Enabled);
-        ImGui.Checkbox("MODO PRUEBA (Usa Sprint en cualquier zona)", ref Conf.TestMode);
-        if (Conf.Enabled)
+        public override HashSet<uint> ValidTerritories => new HashSet<uint>(); 
+        public override Metadata Metadata => new Metadata(1, "CantLoad");
+
+        private Config Conf => Controller.GetConfig<Config>();
+        private const uint THUNDER_ID = 466;
+        private const uint TEST_ID = 50; 
+        private HashSet<uint> _markedPlayers = new HashSet<uint>();
+        private bool _active = false;
+
+        public override void OnSettingsDraw()
         {
-            ImGui.TextColored(new System.Numerics.Vector4(1f, 1f, 0f, 1f), "Aviso: Si varios jugadores usan este script, pueden entrar en conflicto con las marcas.");
-        }
-    }
-
-    public class Config : IEzConfig
-    {
-        public bool Enabled = true;
-        public bool TestMode = false;
-    }
-
-    public override void OnUpdate()
-    {
-        if (!Svc.ClientState.IsLoggedIn || Svc.Party.Length == 0 || !Conf.Enabled) return;
-
-        uint statusId = Conf.TestMode ? TEST_ID : THUNDER_ID;
-
-        // Buscar jugadores con el debuff
-        var thunderPlayers = Svc.Party
-            .Where(p => p.GameObject is IBattleChara chara && chara.StatusList.Any(s => s.StatusId == statusId))
-            .Select(p => p.GameObject as IPlayerCharacter)
-            .Where(p => p != null)
-            .OrderBy(p => p.GetRole()) 
-            .ThenBy(p => p.Name.ToString())
-            .ToList();
-
-        if (thunderPlayers.Count > 0)
-        {
-            _active = true;
-            for (int i = 0; i < thunderPlayers.Count && i < 2; i++)
+            ImGui.Checkbox("Habilitar marcado automatico de Thunder", ref Conf.Enabled);
+            ImGui.Checkbox("MODO PRUEBA (Usa Sprint en cualquier zona)", ref Conf.TestMode);
+            if (Conf.Enabled)
             {
-                var player = thunderPlayers[i];
-                if (!_markedPlayers.Contains(player.EntityId))
-                {
-                    string markerType = (i == 0) ? "attack1" : "attack2";
-                    Svc.Commands.ProcessCommand($"/marker {markerType} \"{player.Name}\"");
-                    _markedPlayers.Add(player.EntityId);
-                }
+                ImGui.TextColored(new Vector4(1f, 1f, 0f, 1f), "Aviso: Evita que varios jugadores usen el script a la vez.");
             }
         }
 
-        // Limpiar marcas si el jugador ya no tiene el debuff
-        if (_active)
+        public class Config : IEzConfig
         {
-            var toRemove = new List<uint>();
-            foreach (var entityId in _markedPlayers)
+            public bool Enabled = true;
+            public bool TestMode = false;
+        }
+
+        public override void OnUpdate()
+        {
+            if (!Svc.ClientState.IsLoggedIn || Svc.Party.Length == 0 || !Conf.Enabled) return;
+
+            uint statusId = Conf.TestMode ? TEST_ID : THUNDER_ID;
+
+            var thunderPlayers = Svc.Party
+                .Where(p => p.GameObject is IBattleChara chara && chara.StatusList.Any(s => s.StatusId == statusId))
+                .Select(p => p.GameObject as IPlayerCharacter)
+                .Where(p => p != null)
+                .OrderBy(p => p.GetRole()) 
+                .ThenBy(p => p.Name.ToString())
+                .ToList();
+
+            if (thunderPlayers.Count > 0)
             {
-                if (!thunderPlayers.Any(p => p.EntityId == entityId))
+                _active = true;
+                for (int i = 0; i < thunderPlayers.Count && i < 2; i++)
                 {
-                    var player = Svc.Objects.FirstOrDefault(o => o.EntityId == entityId);
-                    if (player != null)
+                    var player = thunderPlayers[i];
+                    if (!_markedPlayers.Contains(player.EntityId))
                     {
-                        Svc.Commands.ProcessCommand($"/marker off \"{player.Name}\"");
+                        string markerType = (i == 0) ? "attack1" : "attack2";
+                        Svc.Commands.ProcessCommand("/marker " + markerType + " \"" + player.Name + "\"");
+                        _markedPlayers.Add(player.EntityId);
                     }
-                    toRemove.Add(entityId);
                 }
             }
 
-            foreach (var id in toRemove)
+            if (_active)
             {
-                _markedPlayers.Remove(id);
-            }
+                List<uint> toRemove = new List<uint>();
+                foreach (uint entityId in _markedPlayers)
+                {
+                    if (!thunderPlayers.Any(p => p.EntityId == entityId))
+                    {
+                        var player = Svc.Objects.FirstOrDefault(o => o.EntityId == entityId);
+                        if (player != null)
+                        {
+                            Svc.Commands.ProcessCommand("/marker off \"" + player.Name + "\"");
+                        }
+                        toRemove.Add(entityId);
+                    }
+                }
 
-            if (_markedPlayers.Count == 0 && thunderPlayers.Count == 0)
+                foreach (uint id in toRemove)
+                {
+                    _markedPlayers.Remove(id);
+                }
+
+                if (_markedPlayers.Count == 0 && thunderPlayers.Count == 0)
+                {
+                    _active = false;
+                }
+            }
+        }
+
+        public override void OnDirectorUpdate(DirectorUpdateCategory category)
+        {
+            if (category == DirectorUpdateCategory.Wipe || category == DirectorUpdateCategory.Commence)
             {
+                if (_markedPlayers.Count > 0)
+                {
+                    Svc.Commands.ProcessCommand("/marker clear");
+                }
+                _markedPlayers.Clear();
                 _active = false;
             }
-        }
-    }
-
-    public override void OnDirectorUpdate(DirectorUpdateCategory category)
-    {
-        // Limpiar todo en caso de wipe o inicio de pelea
-        if (category == DirectorUpdateCategory.Wipe || category == DirectorUpdateCategory.Commence)
-        {
-            if (_markedPlayers.Count > 0)
-            {
-                Svc.Commands.ProcessCommand("/marker clear");
-            }
-            _markedPlayers.Clear();
-            _active = false;
         }
     }
 }
